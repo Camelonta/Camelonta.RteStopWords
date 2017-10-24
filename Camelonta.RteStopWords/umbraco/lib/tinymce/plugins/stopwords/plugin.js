68fa39112e679e357ffbf3ab295453293d630115
MrSimon <simon.elmqvist@gmail.com>
2017-10-24 17:46:05 +0200

    initial setup

--- Camelonta.RteStopWords/umbraco/lib/tinymce/plugins/stopwords/plugin.js
+++ Camelonta.RteStopWords/umbraco/lib/tinymce/plugins/stopwords/plugin.js
@@ -1,18 +1,34 @@
 ﻿tinymce.PluginManager.add('stopwords', function (editor, url) {
 
-    editor.on('focusout', function (e) {
-        var content = editor.getContent();
-        var stopwords = ["planet", "mars"];
-        for (var index = 0; index < stopwords.length; index++) {
-            var currentWord = stopwords[index];
-            var newWord = '<span style="color:#fff;background:red;">' + currentWord + '</span>';
-            var idx = content.indexOf(currentWord);
-            var lastidx = content.lastIndexOf(currentWord);
-            var hasStopClass = content.substr(0, lastidx - 2).endsWith('red;');
+    var stopwords = ["planet", "mars"];
 
-            if (!hasStopClass) {
-                var re = new RegExp(currentWord, "g");
-                content = content.replace(re, newWord);
+    var allIndexesOf = function (val, content) {
+        var idxs = [];
+        while (content.indexOf(val) !== -1) {
+            var idx = content.lastIndexOf(val);
+            idxs.push(idx);
+            content = content.substr(0, idx);
+        }
+        return idxs;
+    };
+
+    editor.on('focusout', function (e) {
+
+        var content = editor.getContent();
+
+        for (var index = 0; index < stopwords.length; index++) {
+
+            var currentWord = stopwords[index];
+            var newWord = '<span style="background:red;">' + currentWord + '</span>';
+
+            var idxs = allIndexesOf(currentWord, content);
+            for (var i = 0; i < idxs.length; i++) {
+                var idx = idxs[i];
+                var hasStopClass = content.substr(0, idx - 2).endsWith('red;');
+                if (!hasStopClass) {
+                    var wordLength = currentWord.length;
+                    content = content.substr(0, idx) + newWord + content.substr(idx + wordLength);
+                }
             }
         }
 
